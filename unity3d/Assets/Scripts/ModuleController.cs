5e95ba65ea1a34957cdf11b4366c39b6a2fd70f8
Jim Jing <jimjing131313@gmail.com>
2015-03-11 00:14:03 -0400

    added new smores module prefab

---
+++ unity3d/Assets/Scripts/ModuleController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModuleController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Collider[] colliders = gameObject.GetComponentsInChildren<Collider>();
+		foreach (Collider collider1 in colliders) {
+			foreach (Collider collider2 in colliders) {
+				if (collider1 != collider2) {
+					Physics.IgnoreCollision(collider1, collider2);
+				}
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
