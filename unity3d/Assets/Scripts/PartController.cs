b922ba5768451c7eccf1e800815d1168c522b5f8
Jim Jing <jimjing131313@gmail.com>
2015-04-25 22:00:55 -0400

    added partcontroller and updated smores prefab

---
+++ unity3d/Assets/Scripts/PartController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class PartController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnJointBreak(float breakForce) {
+        string otherM = gameObject.GetComponent<FixedJoint> ().connectedBody.transform.parent.name;
+
+        EditorUtility.DisplayDialog("Undesired module disconnection detected !",
+                                    string.Format ("Module {0} is disconnected from Module {1}", otherM, transform.parent.name), "Ok");
+    }
+}
